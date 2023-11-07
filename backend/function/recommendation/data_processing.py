614844b1926ab8171ef81669e36393f0eac08fc3
losdeam <3500166558@qq.com>
2023-11-07 16:46:21 +0800

    推荐系统实验失败，使用随机数预测分区

--- backend/function/recommendation/data_processing.py
+++ backend/function/recommendation/data_processing.py
@@ -1,6 +1,6 @@
 from surprise import Dataset
 from surprise import Reader
-
+from  sql import get_values
 
 def dataread(path= ""):
     reader = Reader(line_format='user item rating timestamp', sep=',', skip_lines=1)
