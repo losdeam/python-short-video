9c6e4b3229019c3dc1ef0af363ab879625bc2e41
kom <102006886+komiblog@users.noreply.github.com>
2023-11-07 20:25:08 +0800

    小修改

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,16 @@
-// import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from'../src/components/router'
-import "video.js/dist/video-js.css";
+// 导入路由
+import router from '@/router'
+// 导入http轻量库axios
+import axios from 'axios'
+
+// 配置axios默认域名
+axios.defaults.baseURL = "http://127.0.0.1:5000"
+// 前端cookie请求
+axios.defaults.withCredentials = true
+// 自定义vue属性http
+Vue.prototype.$http = axios
 
 const app = createApp(App)
 
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import Layout from '@/components/Layout.vue';
 import VideoSwitch from '@/components/VideoSwitch.vue';
-
+// 导入组件
 import Dashboard from '@/views/Dashboard.vue';
 
 
@@ -10,8 +10,8 @@ const router = createRouter({
     history: createWebHistory(),
     routes: [
         { path: '/', component: Dashboard, },
-        { path: '/layout', component: Layout, name: "Layout" },
-        { path: '/videoswitch', component: VideoSwitch },
+        // { path: '/layout', component: Layout, name: "Layout" },
+        // { path: '/videoswitch', component: VideoSwitch },
     ]
 })
 
